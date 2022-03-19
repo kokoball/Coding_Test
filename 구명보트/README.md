acdd52865160224b7cb14978629bbc1ee84c17f0
kokoball <kbp4154@naver.com>
2022-03-20 01:16:29 +0900

    구명보트

---
+++ 구명보트/README.md
@@ -0,0 +1,7 @@
+## 프로그래머스 Level 2
+
+## 코딩테스트 연습 탐욕법(Greedy) 구명보트
+
+https://programmers.co.kr/learn/courses/30/lessons/42885
+
+탐욕법, Greedy, for문 2중 조건
---
+++ 구명보트/test.js
@@ -0,0 +1,13 @@
+function solution(people, limit) {
+  people.sort(function (a, b) {
+    return a - b;
+  });
+  for (var i = 0, j = people.length - 1; i < j; j--) {
+    if (people[i] + people[j] <= limit) i++;
+  }
+  return people.length - i;
+}
+
+const people = [70, 50, 80, 50];
+
+console.log(solution(people, 100));

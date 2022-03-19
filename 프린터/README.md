9fcc3624b543459f6f52269b8f61396898a52fba
kokoball <kbp4154@naver.com>
2022-03-19 20:01:59 +0900

    프린터

---
+++ 프린터/README.md
@@ -0,0 +1,7 @@
+## 프로그래머스 Level 2
+
+## 코딩테스트 연습 스택/큐 프린터
+
+https://programmers.co.kr/learn/courses/30/lessons/42587
+
+map으로 객체 형태로 관리, js.property.some(), 스택, 큐
---
+++ 프린터/test.js
@@ -0,0 +1,20 @@
+function solution(priorities, location) {
+  var list = priorities.map((t, i) => ({
+    my: i === location,
+    val: t,
+  }));
+  var count = 0;
+  while (true) {
+    var cur = list.shift();
+    if (list.some((t) => t.val > cur.val)) {
+      list.push(cur);
+    } else {
+      count++;
+      if (cur.my) return count;
+    }
+  }
+}
+
+const arr = [2, 1, 3, 2];
+
+console.log(solution(arr, 2));

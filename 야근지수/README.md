c8756215dc04d6820a28195a91084ce1142ae99d
kokoball <kbp4154@naver.com>
2022-03-26 12:43:28 +0900

    야근지수

---
+++ 야근지수/README.md
@@ -0,0 +1,8 @@
+## 프로그래머스 Level 3
+
+## 코딩테스트 연습 연습문제 야근 지수
+
+https://velog.io/@longroadhome/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-LV.3-%EC%95%BC%EA%B7%BC-%EC%A7%80%EC%88%98-JS
+https://programmers.co.kr/learn/courses/30/lessons/12927
+
+최대값 최적화, sort 반복 제거
---
+++ 야근지수/test.js
@@ -0,0 +1,33 @@
+function solution(n, works) {
+  if (works.reduce((a, b) => a + b) <= n) return 0;
+
+  let sorted = works.sort((a, b) => a - b);
+  const len = works.length;
+
+  while (n) {
+    // 현재 최대값을 지정한다.
+    // 아래의 반복문으로 인해 재정렬 없이
+    // 현재 최대값은 항상 마지막 인덱스에 위치한다.
+    const max = sorted[len - 1];
+
+    // 마지막 원소부터 앞으로 가며
+    // 현재 최대값 이상인 값이 있다면 제거해준다.
+    // 따라서 현재 최대값은 계속 마지막 위치에 있을 수 있다.
+    for (let i = len - 1; i >= 0; i--) {
+      if (sorted[i] >= max) {
+        sorted[i]--;
+        n--;
+      }
+      // 주어진 시간을 다 쓴 경우엔 반복문 탈출
+      if (!n) break;
+    }
+  }
+  // 처리해야할 총 작업량보다 근무시간이 같거나 크다면
+  // 모든 일을 처리할 수 있기에 피로도가 없다.
+  if (works.reduce((a, b) => a + b) <= n) return 0;
+
+  return sorted.reduce((a, b) => a + Math.pow(b, 2), 0);
+}
+const expression = [4, 3, 3];
+
+console.log(solution(4, expression));

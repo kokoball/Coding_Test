a45c58a76dcd17a4a2429ec789646db733d07af4
kokoball <kbp4154@naver.com>
2022-04-14 23:45:19 +0900

    영어 끝말잇기

---
+++ 영어 끝말잇기/README.md
@@ -0,0 +1,5 @@
+## 프로그래머스 Level 2
+
+## 코딩테스트 연습 Summer/Winter Coding(~2018) 영어 끝말잇기
+
+https://programmers.co.kr/learn/courses/30/lessons/12981
---
+++ 영어 끝말잇기/test.js
@@ -0,0 +1,40 @@
+function solution(n, words) {
+  var fail_i = -1;
+  for (var i = 1; i < words.length; i++) {
+    var val = words[i];
+    // 전단계의 끝말과 현단계 첫말이 다를 경우
+    if (
+      words[i - 1].substring(words[i - 1].length - 1) != val.substring(0, 1)
+    ) {
+      fail_i = i;
+      break;
+    }
+    // indexOf 함수는 첫번째로 값이 맞는 인덱스만 반환하므로
+    // 현재 인덱스와 맞지 않을 경우 중복된 값
+    if (words.indexOf(val) != i) {
+      fail_i = i;
+      break;
+    }
+  }
+
+  if (fail_i == -1) return [0, 0];
+
+  var no = (fail_i % n) + 1;
+  var turn = Math.floor(fail_i / n) + 1;
+
+  return [no, turn];
+}
+
+const words = [
+  "tank",
+  "kick",
+  "know",
+  "wheel",
+  "land",
+  "dream",
+  "mother",
+  "robot",
+  "tank",
+];
+
+console.log(solution(3, words));

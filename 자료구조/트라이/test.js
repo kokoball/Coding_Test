1f86a5c291bbf271fea0a42ffcc26ded4b816542
kokoball <kbp4154@naver.com>
2022-04-25 20:13:35 +0900

    트라이

--- 자료구조/트라이/test.js
+++ 자료구조/트라이/test.js
@@ -1,4 +1,6 @@
 // 인접 리스트 형태로 구현
+
+// 트리구조, Node 필요
 class Node {
   constructor(value = "") {
     this.value = value;
@@ -7,18 +9,23 @@ class Node {
 }
 
 class Trie {
+  // root 빈 노드 생성
   constructor() {
     this.root = new Node();
   }
 
+  // 탐색 루트부터 시작
   insert(string) {
     let currentNode = this.root;
 
+    // 문자열을 앞에서부터 하나씩 자르면서 순회
+    // 현재 노드에서 문자열을 간선으로 가지고 있지 않다면 새롭게 노드 추가
     for (const char of string) {
       if (!currentNode.children.has(char)) {
         currentNode.children.set(char, new Node(currentNode.value + char));
       }
 
+      // 다음 정점으로 이동
       currentNode = currentNode.children.get(char);
     }
   }

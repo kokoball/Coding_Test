1d8cbcba1908ea1f5b5e6e976e7c9b7056c20f7e
kokoball <kbp4154@naver.com>
2022-04-26 15:41:17 +0900

    큰 수 만들기

---
+++ 큰 수 만들기/README.md
@@ -0,0 +1,7 @@
+## 프로그래머스 Level 2
+
+## 코딩테스트 연습 탐욕법(Greedy) 큰 수 만들기
+
+https://programmers.co.kr/learn/courses/30/lessons/42883
+
+그리디, stack
---
+++ 큰 수 만들기/test.js
@@ -0,0 +1,21 @@
+const solution = (number, k) => {
+  const stack = [];
+  let count = 0;
+
+  for (const item of number) {
+    while (count < k && stack[stack.length - 1] < item) {
+      stack.pop();
+      count += 1;
+    }
+    stack.push(item);
+  }
+  while (count < k) {
+    stack.pop();
+    count += 1;
+  }
+  return stack.join("");
+};
+
+const number = "1924";
+
+console.log(solution(number, 2));

75d414f6f4651cacfb8a90101645960eb417e9c6
kokoball <kbp4154@naver.com>
2022-04-29 21:56:28 +0900

    r1

---
+++ r1/test.js
@@ -0,0 +1,116 @@
+function permutation(arr, selectNum) {
+  const result = [];
+  if (selectNum === 1) return arr.map((v) => [v]);
+
+  arr.forEach((v, idx, arr) => {
+    const fixed = v;
+    const restArr = arr;
+    const permutationArr = permutation(restArr, selectNum - 1);
+    const combineFix = permutationArr.map((v) => [fixed, ...v]);
+    result.push(...combineFix);
+  });
+  return result;
+}
+
+class MinHeap {
+  constructor() {
+    this.heap = [null];
+  }
+
+  size() {
+    return this.heap.length - 1;
+  }
+
+  getMin() {
+    return this.heap[1] ? this.heap[1] : null;
+  }
+
+  swap(a, b) {
+    [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
+  }
+
+  push(value) {
+    this.heap.push(value);
+    let curIdx = this.heap.length - 1;
+    let parIdx = (curIdx / 2) >> 0;
+
+    while (curIdx > 1 && this.heap[parIdx] > this.heap[curIdx]) {
+      this.swap(parIdx, curIdx);
+      curIdx = parIdx;
+      parIdx = (curIdx / 2) >> 0;
+    }
+  }
+
+  pop() {
+    const min = this.heap[1];
+    if (this.heap.length <= 2) this.heap = [null];
+    else this.heap[1] = this.heap.pop();
+
+    let curIdx = 1;
+    let leftIdx = curIdx * 2;
+    let rightIdx = curIdx * 2 + 1;
+
+    if (!this.heap[leftIdx]) return min;
+    if (!this.heap[rightIdx]) {
+      if (this.heap[leftIdx] < this.heap[curIdx]) {
+        this.swap(leftIdx, curIdx);
+      }
+      return min;
+    }
+
+    while (
+      this.heap[leftIdx] < this.heap[curIdx] ||
+      this.heap[rightIdx] < this.heap[curIdx]
+    ) {
+      const minIdx =
+        this.heap[leftIdx] > this.heap[rightIdx] ? rightIdx : leftIdx;
+      this.swap(minIdx, curIdx);
+      curIdx = minIdx;
+      leftIdx = curIdx * 2;
+      rightIdx = curIdx * 2 + 1;
+    }
+
+    return min;
+  }
+}
+
+function solution(arr, k) {
+  if (arr.length === 1) return [arr[0], arr[0]];
+
+  let answer = 0;
+  let cpArr = permutation(arr, 2);
+
+  const heap = new MinHeap();
+
+  for (let i = 0; i < cpArr.length; i++) {
+    heap.push(("" + cpArr[i][0] + cpArr[i][1]) * 1);
+  }
+
+  for (let i = 0; i < k - 1; i++) {
+    heap.pop();
+  }
+
+  answer = "" + heap.pop();
+
+  return [answer[0], answer[1]];
+}
+
+const arr = [5, 5, 10, 10, 5, 8];
+
+console.log(solution(arr, 4));
+
+// let x = solution(arr, 4);
+
+// console.log(x);
+// console.log(x.pop());
+// console.log(x.pop());
+// console.log(x.pop());
+// console.log(x.pop());
+// console.log(x.pop());
+// console.log(x.pop());
+// console.log(x.pop());
+// console.log(x.pop());
+// console.log(x.pop());
+// console.log(x.pop());
+// console.log(x.pop());
+// console.log(x.pop());

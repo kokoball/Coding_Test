79893ffdbad38fb232bc182be2c90a2dbdcb0c99
kokoball <kbp4154@naver.com>
2022-05-31 15:46:22 +0900

    숫자블록

---
+++ 숫자블록/README.md
@@ -0,0 +1,7 @@
+## 프로그래머스 Level 2
+
+## 코딩테스트 연습 연습문제 숫자 블록
+
+https://programmers.co.kr/learn/courses/30/lessons/12923
+
+큰 약수 구하기
---
+++ 숫자블록/test.js
@@ -0,0 +1,22 @@
+function solution(begin, end) {
+  const arr = new Array(end - begin + 1).fill(0);
+
+  for (let i = begin; i <= end; i++) {
+    arr[i - begin] = getMaxDivisor(i);
+  }
+
+  if (begin === 1) arr[0] = 0;
+
+  return arr;
+}
+
+const getMaxDivisor = (n) => {
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0 && n / i <= 1e7) {
+      return n / i;
+    }
+  }
+  return 1;
+};
+
+console.log(solution(1, 10));

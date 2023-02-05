fa32d0e1668f08e649c8446c15a78c69da941755
Byunghpyo Ko <kbp4154@naver.com>
2023-02-05 18:08:47 +0900

    Update 연속 부분 수열 합의 개수.js

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -3,8 +3,6 @@ function solution(elements) {
     const circularArr = elements.slice().concat(elements)
     
     for(let i = 1; i <= elements.length; i++ ){
-        // const tempArr = elements.slice().concat(elements.slice(0,i))
-        
         for(let j = 0; j < elements.length; j++){
             set.add(circularArr.slice(j, j+i).reduce((a,c) => a+c, 0))
         }

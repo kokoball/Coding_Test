e64132f7091ea264146f73dd8392170306e6758d
Byunghpyo Ko <kbp4154@naver.com>
2023-02-06 22:16:17 +0900

    [level 2] Title: 택배상자, Time: 63.05 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131704. 택배상자/택배상자.js
@@ -0,0 +1,22 @@
+function solution(order) {
+    let answer = 0;
+    const stack = [];
+    let index = 1
+    
+    while(index <= order.length || stack.at(-1) === order[answer]){
+        if(index === order[answer]){
+            answer++
+            index++
+            continue;
+        }
+        if(stack.length > 0 && stack.at(-1) === order[answer]){
+            answer++
+            stack.pop();
+            continue;
+        }
+        stack.push(index);
+        index++
+    }
+ 
+    return answer;
+}

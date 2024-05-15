a64a7fc7a2b8fbc1fc50bd1e7b0abe12a1422727
kokoball <kbp4154@naver.com>
2024-05-15 18:44:43 +0900

    2669

--- 백준/Implementation/2669.js
+++ 백준/Implementation/2669.js
@@ -6,7 +6,7 @@ function solution(input) {
   let totalArea = 0;
 
   input.forEach((rect) => {
-    const [x1, y1, x2, y2] = rect;
+    const [x1, y1, x2, y2] = rect.split(" ").map(Number);
 
     for (let x = x1; x < x2; x++) {
       for (let y = y1; y < y2; y++) {

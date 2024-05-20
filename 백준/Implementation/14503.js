ae01710d068e8ee5a4f311bc23d52273d6ef9a92
kokoball <kbp4154@naver.com>
2024-05-20 18:04:47 +0900

    14503

---
+++ 백준/Implementation/14503.js
@@ -0,0 +1,47 @@
+const filePath = process.platform === "linux" ? "dev/stdin" : "../test.txt";
+const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
+
+function solution(input) {
+  const [n, m] = input.shift().split(" ").map(Number);
+  let [x, y, d] = input.shift().split(" ").map(Number);
+  const arr = input.map((i) => i.split(" ").map(Number));
+  const visited = Array.from({ length: n }, () => Array.from({ length: m }, () => false));
+
+  const dx = [-1, 0, 1, 0];
+  const dy = [0, 1, 0, -1];
+
+  let ans = 0;
+  let cnt = 0;
+
+  while (1) {
+    if (cnt === 4) {
+      const [backx, backy] = [x + dx[(d + 2) % 4], y + dy[(d + 2) % 4]];
+      if (arr[backx][backy] === 1) break;
+      else {
+        x = backx;
+        y = backy;
+        cnt = 0;
+      }
+    }
+
+    if (!visited[x][y]) {
+      visited[x][y] = true;
+      arr[x][y] = -1;
+      ans++;
+    }
+
+    const [nextX, nextY] = [x + dx[(d + 3) % 4], y + dy[(d + 3) % 4]];
+    if (arr[nextX][nextY] === 0) {
+      x = nextX;
+      y = nextY;
+      cnt = 0;
+    } else {
+      cnt++;
+    }
+    d = (d + 3) % 4;
+  }
+
+  return ans;
+}
+
+console.log(solution(input));

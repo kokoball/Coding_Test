01d26fce73d55de47714b205a2fafa2f3f96301b
kokoball <kbp4154@naver.com>
2024-06-04 23:20:21 +0900

    1343

---
+++ 백준/Implementation/1343.js
@@ -0,0 +1,11 @@
+const filePath = process.platform === "linux" ? "dev/stdin" : "../test.txt";
+const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
+
+function solution(input) {
+  let answer = input[0].replace(/XXXX/g, "AAAA").replace(/XX/g, "BB");
+  answer.includes("X") ? (answer = -1) : answer;
+
+  return answer;
+}
+
+console.log(solution(input));

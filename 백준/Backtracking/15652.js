6d7475ad96cdb614936bcf4999f72806524d6b81
kokoball <kbp4154@naver.com>
2024-06-18 19:48:21 +0900

    15652

---
+++ 백준/Backtracking/15652.js
@@ -0,0 +1,24 @@
+const filePath = process.platform === "linux" ? "dev/stdin" : "../test.txt";
+const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
+
+function solution(input) {
+  const [N, M] = input[0].split(" ").map(Number);
+  const seq = new Array(M).fill(0);
+  let result = "";
+
+  function dfs(k) {
+    if (k === M) {
+      return (result += `${seq.join(" ")}\n`);
+    }
+    for (let i = 1; i <= N; i++) {
+      if (k > 0 && seq[k - 1] > i) continue;
+      seq[k] = i;
+      dfs(k + 1);
+    }
+  }
+
+  dfs(0);
+  return result;
+}
+
+console.log(solution(input));

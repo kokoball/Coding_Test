74fee86109fb6aa150d0cce9034a9402a71a082a
kokoball <kbp4154@naver.com>
2024-08-19 01:07:39 +0900

    1316

--- 백준/Implementation/1316.js
+++ 백준/Implementation/1316.js
@@ -1,26 +1,18 @@
-const filePath = process.platform === "linux" ? "dev/stdin" : "../test.txt";
-const input = require("fs")
-  .readFileSync(filePath)
-  .toString()
-  .trim()
-  .split("\n");
-
-function solution(input) {
-  const N = input.shift().split("").map(Number)[0];
-  let answer = 0;
-
-  for (let i = 0; i < N; i++) {
-    const arr = input[i].split("");
-    const setArr = new Set(arr);
-    const temp = [arr[0]];
-
-    for (let j = 1; j < arr.length; j++) {
-      if (arr[j] !== arr[j - 1]) temp.push(arr[j]);
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const set = new Set();
+let count = +input[0];
+for (let i = 1; i <= Number(input[0]); i++) {
+  let str = input[i].split('');
+  for (let j = 0; j < str.length; j++) {
+    if (set.has(str[j])) {
+      if (str[j] !== str[j - 1]) {
+        count--;
+        break;
+      }
+    } else {
+      set.add(str[j]);
     }
-    if (temp.length === setArr.size) answer += 1;
   }
-
-  return answer;
+  set.clear();
 }
-
-console.log(solution(input));
+console.log(count);
